81b796451287073b39d2ce5ae3f9e16c31e29f85
bambang16 <bambangnh16@gmail.com>
2017-03-14 20:56:41 +0700

    Add files via upload

---
+++ Praktek1.cpp
@@ -0,0 +1,28 @@
+//Nama : Bambang Nurhidayat
+//Nim : 16111176
+//Kelas : TIFRM16C
+
+#include<iostream>
+
+using namespace std;
+
+
+int main(){
+
+int a,b,c;
+float f;
+
+a=	10;
+b=	20;
+
+c=a+b;
+
+cout<<c<<endl;
+
+f=70.0/3.0;
+
+cout<<f<<endl;
+
+return 0;
+
+}
---
+++ Praktek2.cpp
@@ -0,0 +1,26 @@
+//Nama : Bambang Nurhidayat
+//Nim : 16111176
+//Kelas : TIFRM16C
+
+#include<iostream>
+#define phi 3.14
+using namespace std;
+
+
+int main(){
+
+
+float jari_jari, luas;
+
+jari_jari=2.5; 
+
+luas= 0.5 * phi * jari_jari * jari_jari;
+
+
+cout<<"Data Lingkaran"<<endl;
+cout<<"N Luas:"<<jari_jari;
+cout<<"\n";
+
+return 0;
+
+}
